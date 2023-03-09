3528dc29682cf328064c1ec841ff4995ded20464
Yana Demchyshyn <yana.demchyshyn@ukr.net>
2023-03-10 00:48:11 +0200

    add API integration

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className='container'>
-        <Weather />
+        <Weather defaultCity="Kyiv"/>
         <footer>
           This project was coded by <a href='https://github.com/YanaDemchyshyn/react-main-project' target="blank">Yana Demchyshyn</a> and is open-sourced on GitHub
         </footer>
--- src/Weather.js
+++ src/Weather.js
@@ -1,45 +1,70 @@
-import React from "react";
+import React, {useState} from "react";
 import "./weather.css"
+import axios from "axios";
 
-export default function Weather() {
-    return(<div className="Weather">
-        <form>
-        <div className="row">
-            <div className="col-9">
-                <input type="search"
-                placeholder="Enter a city..."
-                className="form-control" 
-                autoFocus="on"
-                />
-                </div>
-            <div className="col-3">
-                <input type="submit"
-                value="search"
-                className="btn btn-primary w-100" />
+export default function Weather(props) {
+    const [weatherData, setWeatherData] = useState({ready:false});
+
+    function handleResponse(response) {
+        setWeatherData({
+            ready:true,
+            temperature:response.data.temperature.current,
+            humidity:response.data.temperature.humidity,
+            pressure:response.data.temperature.pressure,
+            wind:response.data.wind.speed,
+            description:response.data.condition.description,
+            city:response.data.city,
+            date:"Wednesday, 7:00",
+            iconUrl:"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAUhJREFUeNrt230NgzAQh2GkIAUJyJgMJCABCZNQKcxBHdyOpFkyEkYGV9oL75Lff2Rwz8pXe2tEpLlzGgAAAAAAAAAAYHsDg4+8Hp1m0ATNrJGNzGmbZdvOZN+lAFLRkyb+KHgvMX1H5wYgFR5OFL2VcATiMgA9uFbzzFD4Oss+2qoA9ID6k0P9yKnRVwGgBzJeWPg6Y1GAdIGSwpmKAFRS/C5CFoDCw/6v08EcIF3wpNL0WQHSrS5WDBDXt0hrgGfFxX+eE7IApCc8cZIuB0BwBBBMAZz9+l+jwApgcggwWQJEhwDRBMDp8P+cBhYAg2OAwQIgOAYIFgCzY4DZAkBcB4C7AywvcIwAAADgNsiDEI/CvAzxOsyECFNiTIoyLc7CCEtjLI6yPE6DBC0yNEnRJkejJK2yNEvTLs8fJgAAAAAAAADg1nkDlR7XfJiH1ggAAAAASUVORK5CYII="
+        });
+    }
+    if (weatherData.ready) { 
+        return(<div className="Weather">
+    <form>
+    <div className="row">
+        <div className="col-9">
+            <input type="search"
+            placeholder="Enter a city..."
+            className="form-control" 
+            autoFocus="on"
+            />
             </div>
+        <div className="col-3">
+            <input type="submit"
+            value="search"
+            className="btn btn-primary w-100" />
         </div>
-        </form>
-        <h1>Kyiv</h1>
-        <ul>
-            <li>Wednesday 7:00</li>
-            <li>Mostly cloudly</li>
-        </ul>
-        <div className="row mt-3">
-            <div className="col-6">
-                <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAjdJREFUeNrtmsGtgzAMhjtCRmAERsgIHYFjjx2BERiBEToCI+TaG2+DbJBnKj8pD1Eaiv8AwpX+C1Ap/hzHjpNLCOFyZl0UgAJQAApAASgABaAAFIACODEA9C/83AypIrUkRwpv5Pib4dsCNp5cAMgIS3rMGPxJw3/t4QDQoEtSt8LwsTpJEFAAAONjNUM4HSIEOO7vJC8Mwa2FkHURZBDSM2KAWu4CwPP5LEgtyZMCy/OzIgKBgGA2BUAGliPDxxreldFM2EU4JAOgwRtSPeHdmj0/Z3z8fcEQasTCiATgEgxMURvNAkR2sOIA2MtBSH60KNbC4dAhAHhBACFDdrBiADj2A0DI7PA4AgB0dihWAaBBXUk92Hhkdqi+BsDGh8ySzg7tGgD9BgC88FrgvgKQIeY/ZgepxXB3m6EFm6bm7ADqQwDggSLUHQVA2KG6P50JQJ+a96UBuC08OwqRerOWGPftkUa7Oc/GrTBKo5b0mEivwzOLAlABjb8v6EY1CXVGgwCAampUqZ5NNP5fuS1aB6w87Zn1PG+XpStOKw3AIro5IONfM0e8EhRsZFxz7EYRACRmgQc0YPMAENrAdJk6UBgAKwojz/W/yQUgXgilARguS5dUd2Yir+foPzSQ7TAfifdLOjZvzhSzQID0A3gmuJTDzIQzRXg4IC9IvFsY68jzWxr/qgvQV2TsRJ1gwMXOIuW+JNWhjtx2DWCDE6fvAOhNUQWgABSAAlAACkABKAAFoABOp1+6Bd0LJ+BorgAAAABJRU5ErkJggg==" 
-                alt="mostly cloudy" />
-                <span className="temp">
-                <span className="temperature">7</span>
-                <span className="unit">°C</span>
-                </span>
-            </div>
-            <div className="col-6">
-                <ul>
-                    <li>Presipitation: 15%</li>
-                    <li>Humidity: 70%</li>
-                    <li>Wind: 13km/h</li>
-                </ul>
-            </div>
+    </div>
+    </form>
+    <h1>{weatherData.city}</h1>
+    <ul>
+        <li>{weatherData.date}</li>
+        <li className="text-capitalize">{weatherData.description}</li>
+    </ul>
+    <div className="row mt-3">
+        <div className="col-6">
+            <img src={weatherData.iconUrl}
+            alt={weatherData.description} />
+            <span className="temp">
+            <span className="temperature">{Math.round(weatherData.temperature)}</span>
+            <span className="unit">°C</span>
+            </span>
         </div>
-    </div>)
+        <div className="col-6">
+            <ul>
+                <li>Pressure: {weatherData.pressure}</li>
+                <li>Humidity: {weatherData.humidity}%</li>
+                <li>Wind: {weatherData.wind}km/h</li>
+            </ul>
+        </div>
+    </div>
+</div>)
+    }
+
+    else {
+    let apiKey = "14b37c21t13746dfa0b52a2b355co69b";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+    }
 }
