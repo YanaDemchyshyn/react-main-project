d9d3952fc2551c9309d2ec99b4b99d908869656e
Yana Demchyshyn <yana.demchyshyn@ukr.net>
2023-03-15 23:56:32 +0200

    add icon from API

--- src/Weather.js
+++ src/Weather.js
@@ -5,7 +5,7 @@ import axios from "axios";
 
 export default function Weather(props) {
     const [weatherData, setWeatherData] = useState({ready:false});
-    const [city, setCity] = useState(props.defaultCity)
+    const [city, setCity] = useState(props.defaultCity);
 
     function handleResponse(response) {
         setWeatherData({
@@ -17,23 +17,25 @@ export default function Weather(props) {
             description:response.data.condition.description,
             city:response.data.city,
             date: new Date (response.data.time * 1000),
-            iconUrl:"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAUhJREFUeNrt230NgzAQh2GkIAUJyJgMJCABCZNQKcxBHdyOpFkyEkYGV9oL75Lff2Rwz8pXe2tEpLlzGgAAAAAAAAAAYHsDg4+8Hp1m0ATNrJGNzGmbZdvOZN+lAFLRkyb+KHgvMX1H5wYgFR5OFL2VcATiMgA9uFbzzFD4Oss+2qoA9ID6k0P9yKnRVwGgBzJeWPg6Y1GAdIGSwpmKAFRS/C5CFoDCw/6v08EcIF3wpNL0WQHSrS5WDBDXt0hrgGfFxX+eE7IApCc8cZIuB0BwBBBMAZz9+l+jwApgcggwWQJEhwDRBMDp8P+cBhYAg2OAwQIgOAYIFgCzY4DZAkBcB4C7AywvcIwAAADgNsiDEI/CvAzxOsyECFNiTIoyLc7CCEtjLI6yPE6DBC0yNEnRJkejJK2yNEvTLs8fJgAAAAAAAADg1nkDlR7XfJiH1ggAAAAASUVORK5CYII="
+            iconUrl:`http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
         });
     }
-    function handleSubmit(event) {
-        event.preventDefault();
-        
-    }
+    
     function search(){
         let apiKey = "14b37c21t13746dfa0b52a2b355co69b";
         let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
         axios.get(apiUrl).then(handleResponse);
     }
 
-    function handleCityChange(event) {
-        setCity(event.target.value);
+    function handleSubmit(event) {
+        event.preventDefault();
         search();
     }
+
+    function handleCityChange(event) {
+        setCity(event.target.value);
+    }
+
 
     if (weatherData.ready) { 
         return(<div className="Weather">
